8e50e18920482caf1f0135953417e92320fd99b8
Oleg Kean <oleg.kean@gmail.com>
2013-02-12 20:19:07 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'sqlite3', '1.3.7'
+gem 'jquery-rails', '2.2.1'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'rspec-rails', '2.12.0'
+  gem 'thin', '1.5.0'
+end
+
+group :test do
+  gem 'webrat', '0.7.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharethought::Application.config.secret_token = 'e84e5869d18708bf5e703de55af3f956287425c339dd1923a9e38f3226112a129667e083e89c7ae139f90227971908f9105065db517f651123381750f79f1616'
