8fb5371812738addbaddf7fe537353465fd724eb
Oleg Kean <oleg.kean@gmail.com>
2013-02-12 21:59:32 +0200

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+  
+  before(:each) do
+    @base_title = "Sharethought | "
+  end
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :home
+      response.should have_selector("title", content: @base_title + "Home")
     end
   end
 
@@ -14,6 +24,11 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get :contact
+      response.should have_selector("title", content: @base_title + "Contact")
+    end
   end
 
   describe "GET 'help'" do
@@ -21,12 +36,22 @@ describe PagesController do
       get 'help'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get :help
+      response.should have_selector("title", content: @base_title + "Help")
+    end
   end
 
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get :about
+      response.should have_selector("title", content: @base_title + "About")
     end
   end
 
